8790052357ab093e19893b9f42c170e8b049a0df
ksy <kseenyoung@gmail.com>
2023-01-11 16:57:16 +0900

    programmers school

---
+++ school/1-1 완주하지 못한 선수(2550).py
@@ -0,0 +1,18 @@
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+
+hash = dict()
+for p in participant:
+    if p in hash:
+        hash[p] += 1
+    else:
+        hash[p] = 1
+
+for c in completion:
+    hash[c] -= 1
+
+for key, value in hash.items():
+    if value == 1:
+        print(key)
+        exit()
