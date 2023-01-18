bb11eb0ad472e18260fc19cc478e66d3108de8a8
ksy <kseenyoung@gmail.com>
2023-01-18 17:19:04 +0900

    programmers school 2 merge Sort

---
+++ school/2-2 상담예약제.py
@@ -0,0 +1,49 @@
+#내 풀이
+booked = [["09:10", "lee"]]
+unbooked = [["09:00", "kim"], ["09:05", "bae"]]
+#
+# visitor = booked + unbooked
+# visitor.sort(key=lambda x: x[0])
+#
+# late = []
+# fin = []
+# current = visitor[0][0]
+#
+# for t, v in visitor:
+#     if t < current:
+#         late.append([t, v])
+#     current += 10
+#     print(current)
+
+
+def timeFu(t):
+    h, m = map(int, t.split(':'))
+    return h*60 + m
+
+
+#풀이
+def solution(booked, unbooked):
+    booked = [(timeFu(t), name) for t, name in booked]
+    unbooked = [(timeFu(t), name) for t, name in unbooked]
+    booked.sort()
+    unbooked.sort()
+    b, u, t, answer = 0, 0, 0, []
+
+    while b < len(booked) and u < len(unbooked):
+
+        t1, t2 = booked[b][0], unbooked[u][0]
+        if t1 <= t:
+            answer.append(booked[b][1])
+            b += 1
+            t += 10
+        elif t2 <= t:
+            answer.append(unbooked[u][1])
+            u += 1
+            t += 10
+        else:
+            t = min(t1, t2)
+
+    return answer
+
+
+print(solution(booked, unbooked))
--- school/2-2 티셔츠.py
+++ school/2-2 티셔츠.py
@@ -3,19 +3,16 @@ people = [2, 3]
 tshirts = [1, 2, 3]
 
 def solution(people, tshirts):
-    answer = len(people)
-    for i in tshirts:
-        if i in people:
-            tshirts.remove(i)
-            people.remove(i)
+    people.sort()
+    tshirts.sort()
+    p = 0
+    for t in tshirts:
+        if p >= len(people):
+            break
+        if t >= people[p]:
+            p += 1
 
-    for i in tshirts:
-        for j in people:
-            if j < i:
-                people.pop()
-                break
-
-    return answer - len(people)
+    return p
 
 
 print(solution(people, tshirts))
