f697e4737a8a975934a9dcb3b1ca117f45b3ecea
kseenyoung <kseenyoung@gmail.com>
2023-02-11 23:47:12 +0900

    [programmers SQL 고득점 Kit] NULL 마무리 & String, Date

--- SQL/SELECT/온라인 판매 데이터 통합하기.sql
+++ SQL/SELECT/온라인 판매 데이터 통합하기.sql
@@ -20,7 +20,8 @@ SALES_DATE	DATE	FALSE
 동일한 날짜, 상품 ID 조합에 대해서는 하나의 판매 데이터만 존재합니다.
 
 문제
-ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품ID, 유저ID, 판매량을 출력하는 SQL문을 작성해주세요. OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요. 결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순, 상품ID까지 같다면 유저 ID를 기준으로 오름차순 정렬해주세요.
+ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품ID, 유저ID, 판매량을 출력하는 SQL문을 작성해주세요. ' ||
+    'OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요. 결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순, 상품ID까지 같다면 유저 ID를 기준으로 오름차순 정렬해주세요.
 
 예시
 예를 들어 ONLINE_SALE 테이블이 다음과 같고
@@ -53,4 +54,10 @@ SALES_DATE	PRODUCT_ID	USER_ID	SALES_AMOUNT
 2022-03-03	2	NULL	1
 2022-03-03	5	5	1
 '''
-
+select left(sales_date, 10), product_id, user_id, sales_amount
+from online_sale
+where mid(sales_date, 3, 5) = '22-03'
+select left(sales_date, 10), product_id, NULL, sales_amount
+from offline_sale
+where mid(sales_date, 3, 5) = '22-03'
+order by sales_date, product_id, user_id
